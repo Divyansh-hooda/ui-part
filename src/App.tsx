48466907a84dd5ee467319f71410447c33b5a5e6
codesterDiv <180194888+codesterDiv@users.noreply.github.com>
2026-08-22 23:14:05 +0530

    modified:   src/App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+// function App() {
+//   return (
+//     <div className="nebula-app">
+//       <h1>Nebula</h1>
+//     </div>
+//   )
+// }
+
+// export default App
+
 function App() {
   return (
-    <div className="nebula-app">
-      <h1>Nebula</h1>
+    <div className="nebula-background">
+      <div className="nebula-app">
+        <h1>Nebula</h1>
+      </div>
     </div>
   )
 }
